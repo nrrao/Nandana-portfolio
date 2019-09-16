d0b173cbda12974873bf419e92d4ca6ab42a4d55
nrrao <nandanarao@yahoo.com>
2019-09-15 23:40:03 -0400

    fixed review comments

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 /* global $ */
-'use strict';
+
 
 
 // Event Listeners
-$("nav").find("a").not( ":last-child" ).click(function(e) {
+$('nav').find('a').not( ':last-child' ).click(function(e) {
   e.preventDefault();
-  let section = $(this).attr("href");
-  $("html, body").animate({
-      scrollTop: $(section).offset().top -50
+  let section = $(this).attr('href');
+  $('html, body').animate({
+    scrollTop: $(section).offset().top - 70
   });
 });
 
 
-$("nav").find("a").last().click(function myFunction() {
-  let x = document.getElementById("myTopNav");
-  let f = document.getElementById("fafa");
-  if (x.className === "desktop-nav") {
-    x.className += " responsive";
-    f.className = "fa fa-close";
+$('nav').find('a').last().click(function myFunction() {
+  let x = document.getElementById('myTopNav');
+  let f = document.getElementById('fafa');
+  if (x.className === 'navbar') {
+    x.className += ' responsive';
+    f.className = 'fa fa-close';
+    
   } else {
-    x.className = "desktop-nav";
-    f.className = "fa fa-bars";
+    x.className = 'navbar';
+    f.className = 'fa fa-bars';
   }
 });
 
